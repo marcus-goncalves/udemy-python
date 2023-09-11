c8969cba8ba9caa8a19ff0eee43f8c58f285aa2b
Marcus Gonçalves <nvrrox@gmail.com>
2023-09-10 21:27:56 -0300

    refact: changing api to serve html

--- car-info/main.py
+++ car-info/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from views.cars_routes import car_router
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"))
 app.include_router(car_router)
+
 
 @app.route("/")
 def home() -> None:
